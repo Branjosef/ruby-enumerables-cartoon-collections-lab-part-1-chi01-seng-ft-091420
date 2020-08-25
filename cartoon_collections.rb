52a3399236942c90223a7489e5e215c2bb3a500b
Brandon Fonnemann <branjosef@gmail.com>
2020-08-25 17:10:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,6 @@ def list_dwarves(array)
   # Print a numbered list of each element
   
   array.each_with_index do |index, dwarves|
-    p "#{index}. #{dwarves}"
+    p "#{index}. *#{dwarves}"
   end
 end
